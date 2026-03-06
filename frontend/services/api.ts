69105046a84145dced459ef7fbff3f780e176d0d
Vinod <vinodlatwal03@gmail.com>
2026-03-07 01:43:11 +0530

    connect frontend with deployed backend

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://personal-book-manager-1byq.onrender.com/api",
+  baseURL: "https://personal-book-manager-1byq.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
